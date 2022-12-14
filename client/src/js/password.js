173be5e8db4ae03569a45f2bbc25368dfdab6998
Alizzie <79795653+Alizzie@users.noreply.github.com>
2022-12-14 15:18:58 +0100

    pruefungsvorleistung checken

--- client/src/js/password.js
+++
@@ -1,9 +0,0 @@
-function showPassword () {
-  const pwordField = document.getElementById('pwordField');
-
-  if (pwordField.type === 'password') {
-    pwordField.type = 'text';
-  } else {
-    pwordField.type = 'password';
-  }
-}
--- server/src/server.mjs
+++ server/src/server.mjs
@@ -3,15 +3,23 @@ import express from 'express';
 
 const server = express();
 server.use(express.json());
-const port = 8080;
 const dirname = path.dirname(process.argv[1]);
 
+// clientseitige statische Daten
 server.use(express.static(path.join(dirname, '../../client/build')));
+
+// Kommandozeile übergebene Port
+let port;
+if (!process.argv[2]) {
+  port = 8080;
+} else {
+  port = process.argv[2];
+}
 
 server.get('/', (request, response) => {
   response.sendFile(path.join(dirname, '../../client/build/index.html'));
 });
 
-server.listen(8080, () => {
-    console.log("Listen on port 8080...");
+server.listen(port, () => {
+  console.log(`Listen on port ${port} ...`);
 });
