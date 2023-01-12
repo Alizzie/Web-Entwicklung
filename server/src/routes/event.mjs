63c8bc90af5dc2045a9e142f11c8c4a6c230e6c5
Alizzie <79795653+Alizzie@users.noreply.github.com>
2023-01-12 02:27:33 +0100

    verschachtelung aufgelöst

--- server/src/routes/event.mjs
+++ server/src/routes/event.mjs
@@ -12,9 +12,7 @@ eventRouter.get('/', (request, response) => {
     if (err) {
       throw err;
     }
-    if (!rows) {
-      console.log('no Events there');
-    }
+
     console.log(rows);
     response.json(rows);
   });
@@ -22,46 +20,68 @@ eventRouter.get('/', (request, response) => {
 
 eventRouter.post('/', (request, response) => {
   const body = request.body;
+
+  // Creating the seatingPlan first, so that we get an auto generated seatingPlan_id
   const seatingPlanData = [body.countTables, body.countTableSeats, body.useBothSides];
   const sqlStmtSeatingPlan = 'INSERT INTO seatingPlan(countTables, seatsPerTable, seatsPerSide) VALUES(?, ?, ?)';
-  // Creating the seatingPlan first, so that we get an auto generated seatingPlan_id
   db.run(sqlStmtSeatingPlan, seatingPlanData, err => {
     if (err) {
       throw err;
     }
   });
 
-  db.get('SELECT last_insert_rowid() as seatingPlanId', (err, row) => {
-    if (err) {
-      throw (err);
-    }
-    const seatingPlanId = row.seatingPlanId;
-    // Creating guestList before Event,so that we get an auto generated guestList_id
-    db.run('INSERT INTO guestList DEFAULT VALUES', (err) => {
+  // Creating new Event
+  getIds().then(x => {
+    console.log('next IDs ', x);
+    const eventData = [body.name, body.date, body.time, x.gId, x.sId, veranstalterId];
+    const sqlStmtEvents = 'INSERT INTO veranstaltungen(name, date,time,guestList_id,seatingPlan_id,veranstalter_id) VALUES(?, ?, ?, ?, ?, ?)';
+    db.run(sqlStmtEvents, eventData, (err) => {
       if (err) {
-        throw err;
+        throw err.message;
       }
-      db.get('SELECT last_insert_rowid() as guestListId', (err, row) => {
-        if (err) {
-          throw err;
-        }
-        const guestListId = row.guestListId;
-        const sqlStmtEvents = 'INSERT INTO veranstaltungen(name, date,time,guestList_id,seatingPlan_id,veranstalter_id) VALUES(?, ?, ?, ?, ?, ?)';
-        const eventData = [body.name, body.date, body.time, guestListId, seatingPlanId, veranstalterId];
-        db.run(sqlStmtEvents, eventData, (err) => {
-          if (err) {
-            throw err.message;
-          }
-        });
-      });
     });
   });
 });
 
+async function getIds () {
+  const sId = await requestSeatingPlanId();
+  const gId = await requestGuestListId();
+  return { sId, gId };
+}
+
+function requestSeatingPlanId () {
+  return new Promise((resolve, reject) => {
+    db.get('SELECT last_insert_rowid() as seatingPlanId', (err, row) => {
+      if (err) {
+        return reject(err);
+      }
+
+      console.log('gotten id' + row.seatingPlanId);
+      return resolve(row.seatingPlanId + 1);
+    });
+  });
+}
+
+function requestGuestListId () {
+  return new Promise((resolve, reject) => {
+    // Creating guestList before Event,so that we get an auto generated guestList_id
+    db.run('INSERT INTO guestList DEFAULT VALUES', (err) => {
+      if (err) {
+        throw reject(err);
+      }
+      db.get('SELECT last_insert_rowid() as guestListId', (err, row) => {
+        if (err) {
+          throw reject(err);
+        }
+        console.log('gotten guestlist', row.guestListId);
+        return resolve(row.guestListId + 1);
+      });
+    });
+  });
+}
+
 eventRouter.delete('/', (request, response) => {
   const sqlStmt = 'DELETE FROM veranstaltungen WHERE veranstaltungen.id = ?';
-  console.log(request.body);
-  console.log(request.body.id);
   db.run(sqlStmt, request.body.id, (err) => {
     if (err) {
       throw err;
